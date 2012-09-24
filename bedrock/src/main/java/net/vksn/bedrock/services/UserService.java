b3be9620293a4eeda151ca8086258b1d2f947f9c
timii <paananen.t@gmail.com>
2012-09-24 13:09:12 +0300

    Cleaned the project and fixes to generic dao

--- bedrock/src/main/java/net/vksn/bedrock/services/UserService.java
+++
@@ -1,8 +0,0 @@
-package net.vksn.bedrock.services;
-
-import net.vksn.bedrock.exceptions.EntityNotFoundException;
-import net.vksn.bedrock.model.User;
-
-public interface UserService extends GenericService<User> {
-	User getUserByUsername(String username) throws EntityNotFoundException;
-}
